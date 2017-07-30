6eb9f237f7235a891de2e8d75f3f25e0a091aefa
annaklym <anna.klymenko2016@gmail.com>
2017-07-30 23:12:36 +0300

    Choose mode

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser());
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
     app.use(express.static(__dirname + '/public'));
     // app.use(express.static(__dirname));
+});
+app.post('/', function (req, res) {
+    //    res.send(JSON.stringify(req.body));
+    res.send(req.body.mode);
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
